e32e43946048d7c45bdeab71bcc13fadbcf07a91
Tynahan <nusilite@gmail.com>
2025-06-26 16:03:58 +0900

    3-2 클래스형, 함수형 컴포넌트 만들기 - 3. React 컴포넌트의 종류 완

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// 클래스 컴포넌트 정의
+// class App extends Component {
+//   render() {
+//     return <div>안녕하세요! 이건 클래스형 컴포넌트입니다.</div>
+//   }
+// }
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>안녕하세요! 이건 함수형 컴포넌트입니다.</div>
   )
 }
 
