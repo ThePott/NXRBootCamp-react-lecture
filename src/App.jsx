b871f309770a1c7d1a5b2ff49f1207af453a4de1
Tynahan <nusilite@gmail.com>
2025-06-26 16:42:21 +0900

    ✨ 4. JSX란 - 햄버거를 이용한 컴포넌트의 제작과 이해

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,71 @@
-import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-// 클래스 컴포넌트 정의
-// class App extends Component {
-//   render() {
-//     return <div>안녕하세요! 이건 클래스형 컴포넌트입니다.</div>
-//   }
-// }
+const Bread = () => {
+  return (
+    <div className="bread">
+      <mark>밀가루</mark>
+      <span>물</span>
+      <span>계란</span>
+      <span>이스트</span>
+    </div>
+  )
+}
+const Patty = () => {
+  return (
+    <div className="patty">
+      <mark>소고기</mark>
+      <span>후추</span>
+      <span>소금</span>
+    </div>
+  )
+}
+const Leaf = () => {
+  return (
+    <div className="leaf">
+      <mark>잎사귀</mark>
+      <span>줄기</span>
+    </div>
+  )
+}
+const Tomato = () => {
+  return (
+    <div className="tomato">
+      <mark>껍질</mark>
+      <span>과육</span>
+      <span>씨앗</span>
+    </div>
+  )
+}
+const Cheese = () => {
+  return (
+
+    <div className="cheese">
+      <mark>우유</mark>
+      <span>소금</span>
+      <span>색소</span>
+    </div>
+  )
+}
+
+const Hamburger = () => {
+  return (
+    <>
+      <Bread />
+      <Patty />
+      <Leaf />
+      <Tomato />
+      <Cheese />
+      <Bread />
+    </>
+  )
+}
 
 const App = () => {
   return (
-    <div>안녕하세요! 이건 함수형 컴포넌트입니다.</div>
+    <>
+      <div>안녕하세요! 이건 함수형 컴포넌트입니다.</div>
+      <Hamburger />
+    </>
   )
 }
 
