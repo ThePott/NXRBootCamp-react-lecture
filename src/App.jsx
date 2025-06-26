9f1f9c879575a7ea88ffe8e382370450415ee3b1
Tynahan <nusilite@gmail.com>
2025-06-26 17:40:25 +0900

    ✨ 3-3 React Props class, 함수형에서 자식에게 내리기

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,41 @@ import './App.css'
 // import React, { Component, useState } from 'react'
 import React, { Component } from 'react'
 
-// // 함수형 컴포넌트에서 상태 관리하기
+// // 함수형 컴포넌트에서 Props로 자식에게 속성 전달하기
+// // 기본적으론 props 라는 객체가 매개변수로 있다. 이를 구조분해할당 하는 게 관례적 표현
+// const CountDisplay = ({count}) => {
+//   return (
+//     <div>이 화면은 Props를 받습니다: {count}</div>
+//   )
+// }
+
 // const App = () => {
 //   const [count, setCount] = useState(0)
 
 //   return (
 //     <>
 //       <div>useState으로 상태를 관리합니다</div>
-//       <div>{count}</div>
+//       <CountDisplay count={count} />
 //       <button onClick={() => setCount((prev) => prev + 1)}>+</button>
 //       <button onClick={() => setCount((prev) => prev - 1)}>-</button>
 //     </>
 //   )
 // }
+
+class CountDisplay extends Component {
+  //// 생략 가능
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    console.log("---- props in class component:", this.props)
+    return (
+      <div>이 화면은 Props를 받습니다: {this.props.counter}</div>
+      // <div>이 화면은 Props를 받습니다: {count}</div>
+    )
+  }
+}
 
 class App extends Component {
   state = { counter: 0 }
@@ -24,7 +46,7 @@ class App extends Component {
     return (
       <>
         <div>asdf</div>
-        <div>count: {this.state.counter}</div>
+        <CountDisplay counter={this.state.counter} />
         <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>+</button>
         <button onClick={() => this.setState({ counter: this.state.counter - 1 })}>-</button>
       </>
